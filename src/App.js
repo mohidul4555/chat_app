9c581213b4a94c2dc926115070c1dd89c1651774
mohidul4555 <127039476+mohidul4555@users.noreply.github.com>
2023-07-31 17:43:07 +0530

    my react chat app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// import { ChatEngine } from 'react-chat-engine';
+// import ChatFeed from './components/ChatFeed';
+// // import LoginForm from './components/LoginForm';
+
+// import './App.css';
+
+// const App = () => {
+//     return (
+//       <ChatEngine
+//         height="100vh"
+//         projectID="0026857c-0fa9-4c2b-9d30-246782ce3c57"
+//         userName="Mohidul"
+//         userSecret="123456"
+//         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+//         // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+//       />
+//     );
+//   }
+  
+//   // infinite scroll, logout, more customizations...
+  
+//   export default App;
+  
+
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-export default App;
+    return (
+      <ChatEngine
+        height="100vh"
+        projectID="0026857c-0fa9-4c2b-9d30-246782ce3c57"
+        // userName="Poulmi"
+        // userSecret="123456"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        // onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
+    );
+  }
+  
+  // infinite scroll, logout, more customizations...
+  
+  export default App;
